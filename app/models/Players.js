003ecd6354d4e33420df047e84084a49bcc85776
Shawn Ellis <seellis1@student.fullsail.edu>
2025-07-14 13:53:17 -0400

    Validation added

--- app/models/Players.js
+++ app/models/Players.js
@@ -3,10 +3,24 @@
 const mongoose = require("mongoose");
 
 const playersSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    jersey: Number,
-    position: String,
+    name: { 
+        type: String,
+        required: true,
+        unique: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    jersey: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    position: { 
+        type: String,
+        required: true
+    },
     team: { type: mongoose.Schema.Types.ObjectId, ref: "Team"},
 });
 
--- app/models/Team.js
+++ app/models/Team.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 
 const teamSchema = new mongoose.Schema({
-    name: String,
-    sport: String,
-    city: String,
+    name: { 
+        type: String,
+        required: true,
+        unique: true,
+    },
+    sport: { 
+        type: String,
+        required: true
+    },
+    city: { 
+        type: String,
+        required: true
+    },
 })
 
 module.exports = mongoose.model('Team', teamSchema)
